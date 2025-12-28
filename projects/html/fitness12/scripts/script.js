e640c6c9135207a5cca43da921a92e27618e90b3
tboyj <jackson.m.philips@gmail.com>
2025-12-27 19:09:57 -0500

    Added fitness 12

---
+++ projects/html/fitness12/scripts/script.js
@@ -0,0 +1,9 @@
+/* Student name: Jackson Philips
+Filename: script.js
+Date: 12/27/25
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
